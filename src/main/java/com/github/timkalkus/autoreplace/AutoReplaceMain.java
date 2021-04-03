ed3949e087901f2eb13aefe4589381281b032117
timkalkus <timkalkus@gmail.com>
2021-04-03 22:38:56 +0200

    working on config

--- src/main/java/com/github/timkalkus/autoreplace/AutoReplaceMain.java
+++ src/main/java/com/github/timkalkus/autoreplace/AutoReplaceMain.java
@@ -1,10 +1,16 @@
 package com.github.timkalkus.autoreplace;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AutoReplaceMain extends JavaPlugin{
+    FileConfiguration config = getConfig();
 
     @Override
     public void onEnable(){
+        config.addDefault("EnabledByDefault",true);
+        config.createSection("IndividualPlayerSettings");
+        config.options().copyDefaults(true);
+        saveConfig();
         getServer().getPluginManager().registerEvents(new AutoReplaceListener(this), this);
     }
 
@@ -12,4 +18,6 @@ public class AutoReplaceMain extends JavaPlugin{
     public void onDisable(){
 
     }
+
+
 }
